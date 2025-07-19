8a125283f61585e6fbe3ff7cc22a42cee2ee0d7d
canadianbishop <aayoola254@gmail.com>
2025-07-19 23:10:43 +0100

    accordion and footer fixed

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-const mobileMenu =document.querySelector('#mobile_menu');
-const menuItems = document.querySelector('#menu_items');
+const mobileMenu = document.querySelector("#mobile_menu");
+const menuItems = document.querySelector("#menu_items");
+const hideBtn = document.querySelectorAll(".minus");
+const showBtn = document.querySelectorAll(".plus");
+const answer = document.querySelectorAll(".answer");
 
-mobileMenu.addEventListener('click',()=>{
-      menuItems.classList.toggle('active')
-})
+mobileMenu.addEventListener("click", () => {
+  menuItems.classList.toggle("active");
+});
+
+showBtn.forEach((ele, i) => {
+  let hide = hideBtn[i];
+  let ans = answer[i];
+
+  ele.addEventListener("click", () => {
+    // hide all the buttons
+
+    showBtn.forEach((btn) => btn.classList.remove("hide"));
+    hideBtn.forEach((btn) => btn.classList.remove("show"));
+    answer.forEach((ans) => ans.classList.remove("show"));
+
+//     open all the current buttons
+
+    ele.classList.toggle("hide");
+    hide.classList.toggle("show");
+    ans.classList.toggle("show");
+
+  });
+});
+
+hideBtn.forEach((ele, i) => {
+  let show = showBtn[i];
+  let ans = answer[i];
+
+  ele.addEventListener("click", () => {
+    ele.classList.remove("show");
+    show.classList.remove("hide");
+    ans.classList.remove("show");
+  });
+});
