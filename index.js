73bba65dff11f3a9092dfb18a3e73e846b3afad4
canadianbishop <aayoola254@gmail.com>
2025-07-20 16:15:12 +0100

    responsiveness and navigation fixed

--- index.js
+++ index.js
@@ -3,10 +3,36 @@ const menuItems = document.querySelector("#menu_items");
 const hideBtn = document.querySelectorAll(".minus");
 const showBtn = document.querySelectorAll(".plus");
 const answer = document.querySelectorAll(".answer");
+const aboutBtn = document.querySelector(".about");
+const homeBtn = document.querySelector(".home");
+const blogBtn = document.querySelector(".blog");
+const loginBtn = document.querySelector(".Login_btn");
 
 mobileMenu.addEventListener("click", () => {
   menuItems.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
 });
+
+homeBtn.addEventListener("click", () => {
+  menuItems.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
+
+aboutBtn.addEventListener("click", () => {
+  menuItems.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
+
+blogBtn.addEventListener("click", () => {
+  menuItems.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
+loginBtn.addEventListener("click", () => {
+  menuItems.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
+
+// accordion
 
 showBtn.forEach((ele, i) => {
   let hide = hideBtn[i];
@@ -19,12 +45,11 @@ showBtn.forEach((ele, i) => {
     hideBtn.forEach((btn) => btn.classList.remove("show"));
     answer.forEach((ans) => ans.classList.remove("show"));
 
-//     open all the current buttons
+    //     open all the current buttons
 
     ele.classList.toggle("hide");
     hide.classList.toggle("show");
     ans.classList.toggle("show");
-
   });
 });
 
